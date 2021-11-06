9ba2d5a1db44f86f97144e9cdc64e300f4779d8b
Luk13Mad <56697676+Luk13Mad@users.noreply.github.com>
2021-11-06 12:05:54 +0100

    established db_connection to database

--- src/configuration.rs
+++ src/configuration.rs
@@ -14,14 +14,23 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
-pub fn get_configuration() -> Result<Settings, config:: ConfigError> {
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Initialise our configuration reader
-    let mut settings = config::Config:: default();
+    let mut settings = config::Config::default();
     // Add configuration values from a file named `configuration`.
     // It will look for any top-level file with an extension
     // that `config` knows how to parse: yaml, json, etc.
-    settings.merge(config::File:: with_name("configuration"))?;
+    settings.merge(config::File::with_name("configuration"))?;
     // Try to convert the configuration values it read into
     // our Settings type
     settings.try_into()
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::net::TcpListener;
-use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = format! ("127.0.0.1:{}", configuration.application_port);
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();

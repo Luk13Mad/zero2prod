7ecfc76c9d7e404bb362167d002baa8f6a33405d
Luk13Mad <56697676+Luk13Mad@users.noreply.github.com>
2021-12-22 15:17:07 +0100

    troubleshooting docker

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,6 +11,7 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
+    env_logger::Builder:: from_env(Env:: default().default_filter_or("info")).init();
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,16 @@
 use crate::routes;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware:: Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
+
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(connection.clone())
